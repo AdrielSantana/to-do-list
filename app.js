9221347248d0e7ceccf7add73407516156c96916
Adriel Santana <diel_san@hotmail.com>
2022-08-14 19:30:09 -0300

    acoplamento com o mongoose usando mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+require('./config/database')
+const path = require('path')
 
 const checkListRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
+
 
 const app = express()
 
 app.use(express.json())
 
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/',rootRouter)
 app.use('/checklists',checkListRouter)
 
 app.listen(3000, () => {
